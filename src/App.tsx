badc8025d24bc89007c179ea380902fbf85bfd99
이준용 <iiiuooyoo@gmail.com>
2023-08-22 18:01:42 +0900

    feat : 버튼 및 블러 백그라운드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import styled from "@emotion/styled";
+
+const OKButton = styled.button`
+    font-size: 2vw !important;
+`;
+
+const BlurBackground = styled.div`
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+    backdrop-filter: blur(2.5px);
+    position: absolute;
+`;
+
 function App() {
-    return <>Hello World!</>;
+    return (
+        <>
+            <OKButton className="kbc-button">Make everything OK</OKButton>
+            <BlurBackground />
+        </>
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "keyboard-css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
