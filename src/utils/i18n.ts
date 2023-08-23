f762d66a00dd8e1481cf24c360a46b49516c1584
이준용 <iiiuooyoo@gmail.com>
2023-08-23 16:02:15 +0900

    update : 한국어 문장 변경

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -15,10 +15,10 @@ const resources = {
     },
     ko: {
         translation: {
-            okButtonText: "모든 것을 괜찮게",
-            progressText: "모든 것을 괜찮게 만드는 중이야",
-            answerTitle: "지금 모든 것이 괜찮아졌어",
-            answerText: `만약 아직 괜찮지 않다면,
+            okButtonText: "전부 해결",
+            progressText: "전부 해결하는 중이야",
+            answerTitle: "지금 전부 해결됐어",
+            answerText: `만약 아직 해결되지 않았다면,
             너가 바라보는 세상의 시각이나
             관점을 조금 바꿔보는 건 어때?`,
             answerButtonText: "계속",
